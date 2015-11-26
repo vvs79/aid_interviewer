d2881709e48fa8d8b827c2550989b98300937274
vvs79 <valentyn-vvs@rambler.ru>
2015-11-26 09:12:43 +0200

    second

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -10,14 +10,20 @@ skip_before_action :verify_authenticity_token
   def about
   end
 
-      def client
-  	  end
+  def client
+  end
 
-def client_post
-	   @client = params[:client]
-	   params[:id] = @client[:name]
-	   render text: 'name - '+@client[:name]+' ,   email - '+@client[:email]
-    # render text: "Client post"
-  	  end
+  def client_post
+	  @client = params[:client]
+	  # params[:id] = @client[:name]
+	  if (@client[:name]=='admin' && @client[:password]=='654321') ||
+       (@client[:email]=='admin@email.com' && @client[:password]=='654321')  
+	     render 'client' and return
+	     #render text: 'name - '+@client[:name]+' ,   email - '+@client[:email] and return
+	  else 
+	    flash.now[:error] = "You are not admin or interviewer"
+      render 'home' # render text: 'INCORRECT'
+	  end
+  end
 
 end
